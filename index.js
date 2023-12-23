ff2f753ae9626546ecdbef234c272f629cbd10a6
Dipu Das <dasdipu8984@gmail.com>
2023-12-24 02:37:40 +0530

    js file changes

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 function onClickNavManuToggle(){
-    console.log('onClickNavManu====')
     document.getElementById("nav-menu-btn").classList.toggle('is-active');
-    let body_ele = document.body
-    body_ele.classList.toggle("nav-hidden")
+    document.body.classList.toggle("nav-hidden")
 }
